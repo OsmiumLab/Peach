230b0055ab92b507e92a819e6990240fe0d082dc
TheKingL <josukejoestar84@gmail.com>
2022-12-02 06:01:23 +0100

    error + htaccesses

--- src/View/error.php
+++ src/View/error.php
@@ -1,13 +1,22 @@
-<style type="text/css">
-    body {
-        background-color: blue;
-    }
+<html>
+    <head>
+        <title>Erreur 404</title>
+        <style type="text/css">
+            body {
+                background-image: url('../../../Peach/web/assets/js/images/backfull.png');
+                background-repeat: no-repeat;
+                background-position: center;
+            }
 
-    h1 {
-        font-family: 'VT323', monospace;
-        color: #fff;
-        text-align: center;
-    }
-</style>
-
-<h1>Erreur 404</h1>
+            h1 {
+                font-family: 'VT323', monospace;
+                color: #000000;
+                background-color: hotpink;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Erreur 404</h1>
+    </body>
+</html>
