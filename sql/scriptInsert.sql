3db85abd4aa975e7d89d736ec4a4d7535d491374
TheKingL <josukejoestar84@gmail.com>
2022-12-02 07:09:09 +0100

    end game

--- sql/scriptInsert.sql
+++ sql/scriptInsert.sql
@@ -1,24 +1,24 @@
 -- MEUF DE LA PHARMACIE
-CALL NDIAJOUTERMESSAGESFRUITS('Merci de votre achat !');
-CALL NDIAJOUTERFRUITS('Eliza', 0, 0, 1);
+CALL NDIAJOUTERMESSAGESFRUITS(0, 'Merci de votre achat !');
+CALL NDIAJOUTERFRUITS(0, 'Eliza', 0, 0, 1);
 
 -- ORANGE
-CALL NDIAJOUTERMESSAGESFRUITS('Si tu trouves un autre fruit, n hesite pas à prendre une pelure chez le primeur au préalable');
-CALL NDIAJOUTERFRUITS('Acer', 63, 2, 2);
+CALL NDIAJOUTERMESSAGESFRUITS(0,'Si tu trouves un autre fruit, n hesite pas à prendre une pelure chez le primeur au préalable');
+CALL NDIAJOUTERFRUITS(0, 'Acer', 63, 2, 2);
 
 -- POIRE
-CALL NDIAJOUTERMESSAGESFRUITS('Ahahah tu es très drôle !');
-CALL NDIAJOUTERFRUITS('Allyn', 10, 0, 3);
+CALL NDIAJOUTERMESSAGESFRUITS(0, 'Ahahah tu es très drôle !');
+CALL NDIAJOUTERFRUITS(0, 'Allyn', 10, 0, 3);
 
 -- KIWI
-CALL NDIAJOUTERMESSAGESFRUITS('Je te conseille de tester la confiture du primeur, apparemment il y a des vers de terre en cavale');
-CALL NDIAJOUTERFRUITS('Amari', 0,  0, 4);
+CALL NDIAJOUTERMESSAGESFRUITS(0, 'Je te conseille de tester la confiture du primeur, apparemment il y a des vers de terre en cavale');
+CALL NDIAJOUTERFRUITS(0, 'Amari', 0,  0, 4);
 
 -- TOMATE
-CALL NDIAJOUTERMESSAGESFRUITS('Je trouve ça très intéressant ce que tu viens de me dire...');
-CALL NDIAJOUTERFRUITS('Aphra', 56, 7, 5);
+CALL NDIAJOUTERMESSAGESFRUITS(0, 'Je trouve ça très intéressant ce que tu viens de me dire...');
+CALL NDIAJOUTERFRUITS(0, 'Aphra', 56, 7, 5);
 
 -- POMME
-CALL NDIAJOUTERMESSAGESFRUITS('Ah non c est pas vrai... tu es Taureau !');
-CALL NDIAJOUTERFRUITS('Ash', 45, 3, 6);
+CALL NDIAJOUTERMESSAGESFRUITS(0, 'Ah non c est pas vrai... tu es Taureau !');
+CALL NDIAJOUTERFRUITS(0, 'Ash', 45, 3, 6);
 
---
+++ src/View/end.php
@@ -0,0 +1,111 @@
+<html>
+    <head>
+        <title>Peach and Pips</title>
+        <style type="text/css">
+            body {
+                background-image: url('../../../Peach/web/assets/js/images/backfull.png');
+                background-repeat: no-repeat;
+                background-position: center;
+            }
+
+            div {
+                margin: 4px;
+            }
+
+            img {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                background-color: hotpink;
+                border: 2px solid black;
+            }
+
+            a {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                background-color: hotpink;
+                border: 2px solid black;
+                text-decoration: none;
+                color: pink;
+                font-family: 'VT323', monospace;
+                font-size: 20px;
+                text-align: center;
+            }
+
+            .flex {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .align {
+                font-family: 'VT323', monospace;
+                color: #000000;
+                background-color: hotpink;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <!--PHARMACIE-->
+        <div class="flex">
+            <img src="../../web/assets/js/images/Sante.png">
+            <div class="align">
+                <p>La Pharmacie</p>
+                <p>L'endoit où vous pourrez acheter des préservatifs, aller demander conseil pour un dépistage et des informations sur les maladies sexuellement transmissibles. Renseignez-vous !</p>
+                <a href="https://sante.journaldesfemmes.fr/fiches-maladies/2813405-premiers-symptomes-sida-vih-apparition-femme-homme-peau/">LIEN</a>
+            </div>
+        </div>
+
+        <!--ORANGE-->
+        <div class="flex">
+            <img src="../../web/assets/js/images/Orange4.png">
+            <div class="align">
+                <p>L'Orange</p>
+                <p>Il avait un taux d'avoir le Sida haut mais peu de relations sexuelles avec d'autres. Se protéger via des préservatifs est une bonne solution !</p>
+                <a href="https://www.moncorpsmacontraception.fr/methodes-contraceptives/preservatif-masculin">LIEN HOMME</a>
+            </div>
+        </div>
+
+        <!--POIRE-->
+        <div class="flex">
+            <img src="../../web/assets/js/images/Poire4.png">
+            <div class="align">
+                <p>La Poire</p>
+                <p>Avoir un taux d'avoir le Sida faible ainsi qu'un nombre de relations sexuelles faible reste tout de même risqué. Pour en avoir le coeur net, dépistez-vous !</p>
+                <a href="https://www.maxisciences.com/sida/depistage-vih-definition-deroulement-de-l-examen-delai-de-resultat_art39775.html">LIEN</a>
+            </div>
+        </div>
+
+        <!--KIWI-->
+        <div class="flex">
+            <img src="../../web/assets/js/images/Kiwi4.png">
+            <div class="align">
+                <p>Le Kiwi</p>
+                <p>Avoir un taux d'avoir le Sida nul ainsi qu'un nombre de relations sexuelles nul reste tout de même risqué. Pour en avoir le coeur net, dépistez-vous !</p>
+                <a href="https://www.ameli.fr/assure/sante/themes/mst-ist/depistage">LIEN</a>
+            </div>
+        </div>
+
+        <!--TOMATE-->
+        <div class="flex">
+            <img src="../../web/assets/js/images/Tomate4.png">
+            <div class="align">
+                <p>La Tomate</p>
+                <p>Avoir un taux élevé d'avoir le Sida ainsi qu'un grand nombre de relations sexuelles est risqué vis-à-vis de la séroposivité. Renseignez vous sur le Sida !</p>
+                <a href="https://www.pasteur.fr/fr/centre-medical/fiches-maladies/sida-vih">LIEN</a>
+            </div>
+        </div>
+
+        <!--POMME-->
+        <div class="flex">
+            <img src="../../web/assets/js/images/Pomme4.png">
+            <div class="align">
+                <p>La Pomme</p>
+                <p>Elle avait un taux d'avoir le Sida haut et peu de relations sexuelles avec d'autres. Se protéger via des préservatifs est une bonne solution !</p>
+                <a href="https://www.moncorpsmacontraception.fr/methodes-contraceptives/preservatif-feminin">LIEN FEMME</a>
+            </div>
+        </div>
+    </body>
+</html>
